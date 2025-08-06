ab233a9ad5669a2e97f04ecdc669d99c5e48d76d
James Hetherington <j.hetherington@ucl.ac.uk>
2025-08-06 23:03:28 +0100

    Don't use the simp copout

--- Tall/ch2.lean
+++ Tall/ch2.lean
@@ -10,10 +10,35 @@ import Mathlib.Algebra.Group.Basic
 
 theorem rearrange_equation  (x: ℚ) (c: ℕ) : 0 ≤ x ∧ x = c → ∃ (a: ℕ )(b: ℕ), a=c * b := by
     intro ht
+    obtain ⟨zltx, h⟩ := ht
+
+    let nq := (x.num : ℚ ) -- we have to work in Q to be able to use
+    let dq := (x.den : ℚ)        -- theorems on multiplying up.
+
+    have dqnz : dq ≠ 0 := Nat.cast_ne_zero.mpr x.den_nz
+
+    -- Now we're in Q and can rearrange the equation
+    -- using num_div_den for the definition of rational numbers.
+    have h_mul : nq =  c * dq := by
+      rw [<- Rat.num_div_den x] at h
+      field_simp [dqnz] at h
+      assumption
+
+
+    -- the critical thing we need to prove is that nq = x.num.toNat
+    have nnnz: nq = x.num.toNat := by
+      dsimp [nq]
+      have nnz: x.num = x.num.toNat := by
+        simp [Int.toNat_of_nonneg, x.num_nonneg]
+        assumption
+      norm_cast
 
     use x.num.toNat, x.den
 
-    simp [*] at *
+    dsimp [nq,dq] at h_mul
+    norm_cast at h_mul
+    simp only [h_mul, nnnz]
+    norm_cast
 
 
 theorem root_2_irrational : ¬∃ (p q : ℤ), q ≠ 0 ∧ (p / q) ^ 2 = 2 := by
